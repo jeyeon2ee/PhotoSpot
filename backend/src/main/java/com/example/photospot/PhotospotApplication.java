b30bac93eedf791702538a36a9facc8055dca4f4
jeyeon2ee <tkachdtk0903@snu.ac.kr>
2021-03-29 23:59:43 +0900

    initialize

--- backend/src/main/java/com/example/photospot/PhotospotApplication.java
+++ backend/src/main/java/com/example/photospot/PhotospotApplication.java
@@ -1,9 +1,14 @@
 package com.example.photospot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class PhotospotApplication {
 
 	public static void main(String[] args) {
