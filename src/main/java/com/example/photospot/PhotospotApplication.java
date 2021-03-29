1b410516edd362086568c2e591d5b491fc09290f
jeyeon2ee <tkachdtk0903@snu.ac.kr>
2021-03-29 22:38:09 +0900

    temp

---
+++ src/main/java/com/example/photospot/PhotospotApplication.java
@@ -0,0 +1,13 @@
+package com.example.photospot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotospotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotospotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photospot/PhotospotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photospot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotospotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
