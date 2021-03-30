5ae4b163daa8344db68ed43e4825473c5ea255c7
jeyeon2ee <tkachdtk0903@snu.ac.kr>
2021-03-30 23:05:16 +0900

    init_수정

--- backend/photospot/photospot/src/main/java/com/example/photospot/PhotospotApplication.java
+++ backend/photospot/photospot/src/main/java/com/example/photospot/PhotospotApplication.java
@@ -2,21 +2,13 @@ package com.example.photospot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class PhotospotApplication {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(PhotospotApplication.class, args);
-
-	}
-
-	@GetMapping
-	public String HelloWorld(){
-		return "welcome";
 	}
 
 }
