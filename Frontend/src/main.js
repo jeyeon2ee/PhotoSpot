c4d5ceb22184413c0faa9bc6799afc1643ce7cce
lactokyun <kyunblue@hanyang.ac.kr>
2021-04-26 20:57:43 +0900

    fontawesome 패키지 설치

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 3rd party library
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
