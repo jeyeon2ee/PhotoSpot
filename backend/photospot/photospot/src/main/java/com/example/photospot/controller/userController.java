8a81abc76b03383ebf83da82563fe165da84dbb7
jeyeon2ee <tkachdtk0903@snu.ac.kr>
2021-05-16 19:47:04 +0900

    logout 추가

--- backend/photospot/photospot/src/main/java/com/example/photospot/controller/userController.java
+++ backend/photospot/photospot/src/main/java/com/example/photospot/controller/userController.java
@@ -35,6 +35,14 @@ public class userController {
     public String login(){
         return "/login";
     }
+    @GetMapping
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+        return "redirect:/";
+    }
 
 }
